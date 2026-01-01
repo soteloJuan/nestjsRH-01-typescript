9c4c71078acbf7889c1da75260a10013182919ec
soteloJuan <torisotelo.juan@gmail.com>
2026-01-01 15:34:05 -0800

    metodos asincronos

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import type { Move, PokeapiResponse } from '../iinterfaces/pokeapi-response.interface';
+
 export class Pokemon { 
     public id: number;
     public name: string;
@@ -37,13 +40,24 @@ export class PokemonCorta {
         console.log(`${this.name}, ${this.name}`);
     }
 
+    async getMoves(): Promise<Move[]>{ // Aqui lo que podemos hacer es creear los types, y de esta manera podemos recibir ayuda por parte de typescript
+        
+
+        const {data} = await axios.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/Charmander');
+        console.log('Response: ', data.moves);
+        return data.moves;
     
+    }
 }
 
 
 export const charmander = new PokemonCorta(1, 'charmander');
 
-console.log(charmander.imageUrl)
+// console.log(charmander.imageUrl)
 
-charmander.scream();
-charmander.speak();
+// charmander.scream();
+// charmander.speak();
+// console.log(charmander.getMoves());
+
+charmander.getMoves();
+
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { charmander } from './bases/03-classes.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript with fernando herrera</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+<h1> Hello ${charmander.name}  ${charmander.id}!</h1>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
